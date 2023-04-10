cb31e60a44317688f69e5bcde38df16863853f80
sev <rudakoff.v@gmail.com>
2023-04-09 21:30:43 -0700

    move locator from the method

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,9 +2,9 @@ import logging
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
-
 import conftest
-from helper.locator import Locator
+
+LOADING_LOGO = By.CSS_SELECTOR, ".pg-loading-logo"
 
 
 class BasePage(object):
@@ -18,6 +18,6 @@ class BasePage(object):
         self.driver.get(self.url)
         self.driver.implicitly_wait(0)
         WebDriverWait(self.driver, 10).until(
-            EC.invisibility_of_element_located((By.CSS_SELECTOR, ".pg-loading-logo")))
+            EC.invisibility_of_element_located(LOADING_LOGO))
         self.driver.implicitly_wait(conftest.IMPLICIT_WAIT_TIME)
         return self
