a6b30d272265aae88c9a3be6b71651116173d7d8
sev <rudakoff.v@gmail.com>
2023-04-09 21:11:27 -0700

    add base page class

---
+++ pages/base_page.py
@@ -0,0 +1,23 @@
+import logging
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+import conftest
+from helper.locator import Locator
+
+
+class BasePage(object):
+    url = None
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def go(self):
+        logging.info(f"Open url: {self.url}")
+        self.driver.get(self.url)
+        self.driver.implicitly_wait(0)
+        WebDriverWait(self.driver, 10).until(
+            EC.invisibility_of_element_located((By.CSS_SELECTOR, ".pg-loading-logo")))
+        self.driver.implicitly_wait(conftest.IMPLICIT_WAIT_TIME)
+        return self
